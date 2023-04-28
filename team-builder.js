574727d24f04ca4b384edd67a2a18c61c7f66b12
Jimmy Li <jmyli562@gmail.com>
2023-04-28 16:52:16 -0700

    fixed bug where pokemon type was not displaying properly

--- team-builder.js
+++ team-builder.js
@@ -9,14 +9,25 @@ addPkmBtn.addEventListener("click", addPokemonToTeam);
 fetchPokemonNames()
 
 function displayPokemonImage(){
+    const div = document.getElementById("pokemon-image");
     const img = document.getElementById("image");
+    const p = document.getElementById("name");
+    const p1 = document.getElementById("ability1");
+    const p2 = document.getElementById("ability2")
     const pokemonName = slctDropDown.value; //grabbing the pokemon that the user selected
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
     .then(resp=>resp.json())
     .then((mons)=>{
         img.src = mons.sprites.front_default;
         img.style = "width:300px;height:300px;";
-        div.appendChild(img);
+        p.textContent = `Name: ${pokemonName}\n`;
+        p1.textContent = `Type 1: ${mons.types[0].type.name}`;
+
+        if(mons.types.length === 1){ //checking if the pokemon has only 1. if so set type 2 as `NONE`
+            p2.textContent = `Type 2: None`;
+        }else{ 
+            p2.textContent = `Type 2: ${mons.types[1].type.name}`;
+        }
     })
 }
 
