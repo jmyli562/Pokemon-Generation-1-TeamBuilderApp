52bd8af9e1b74fda8a58f02aac992dff5fba5e41
Jimmy Li <jmyli562@gmail.com>
2023-04-28 20:27:42 -0700

    add ability to choose moves for each pokemon

--- team-builder.js
+++ team-builder.js
@@ -1,5 +1,13 @@
+
 const pkmTeam1 = {};
 const pkmTeam2 = {};
+const div = document.getElementById("pokemon-image");
+const img = document.getElementById("image");
+const p = document.getElementById("name");
+const p1 = document.getElementById("type1");
+const p2 = document.getElementById("type2");
+const p3 = document.getElementById("ability");
+
 const slctDropDown = document.getElementById("pokemon-list"); //getting the dropdown menu
 const addPkmBtn = document.getElementById("add-pokemon"); //getting the add pokemon button
 
@@ -9,25 +17,55 @@ addPkmBtn.addEventListener("click", addPokemonToTeam);
 fetchPokemonNames()
 
 function displayPokemonImage(){
-    const div = document.getElementById("pokemon-image");
-    const img = document.getElementById("image");
-    const p = document.getElementById("name");
-    const p1 = document.getElementById("ability1");
-    const p2 = document.getElementById("ability2")
+
     const pokemonName = slctDropDown.value; //grabbing the pokemon that the user selected
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
     .then(resp=>resp.json())
     .then((mons)=>{
+        const learnedMoves = mons.moves;
         img.src = mons.sprites.front_default;
         img.style = "width:300px;height:300px;";
         p.textContent = `Name: ${pokemonName}\n`;
         p1.textContent = `Type 1: ${mons.types[0].type.name}`;
 
-        if(mons.types.length === 1){ //checking if the pokemon has only 1. if so set type 2 as `NONE`
+        if(mons.types.length === 1){ //checking if the pokemon has only 1 type. if so set type 2 as `NONE`
             p2.textContent = `Type 2: None`;
         }else{ 
             p2.textContent = `Type 2: ${mons.types[1].type.name}`;
         }
+
+        p3.textContent = `Ability: ${mons.abilities[0].ability.name}`;
+
+        //displayMoves(learnedMoves);
+    })
+}
+
+function displayMoves(moves){
+    
+    const div = document.getElementById("pokemon-move-list");
+    const move1 = document.getElementById("move1");
+    move1.style = "";
+    const move2 = document.getElementById("move2");
+    move2.style = "";
+    const move3 = document.getElementById("move3");
+    move3.style = "";
+    const move4 = document.getElementById("move4");
+    move4.style = "";
+
+    div.appendChild(move1);
+    div.appendChild(move2);
+    div.appendChild(move3);
+    div.appendChild(move4);
+
+    moves.forEach((move)=>{
+        const option = document.createElement("option");
+        option.value = move.move.name;
+        option.textContent = move.move.name;
+        
+        move1.appendChild(option);
+        move2.appendChild(option);
+        move3.appendChild(option);
+        move4.appendChild(option);
     })
 }
 
@@ -37,7 +75,7 @@ function addPokemonToTeam(){
 
 function fetchPokemonNames(){
     let pokemonNames = [];
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=151%27")
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151%27") //limit the search to 151 pokemon
     .then(resp=>resp.json())
     .then((mons)=>{
         mons.results.forEach((pokemon)=>{
