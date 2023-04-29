c1f2def4ffb687a07cb798dfd81ca1c22dbd7d81
Jimmy Li <jmyli562@gmail.com>
2023-04-28 23:06:38 -0700

    add ability to preview team as the user is adding pokemon

--- team-builder.js
+++ team-builder.js
@@ -1,6 +1,7 @@
-
-const pkmTeam1 = {};
-const pkmTeam2 = {};
+let changedPokemon = false;
+const pkmTeam1 = []; //an array that will hold pokemon objects of the users selected team of pokemon
+const pkmTeam2 = [];
+let currPokemon = {};
 const div = document.getElementById("pokemon-image");
 const img = document.getElementById("image");
 const p = document.getElementById("name");
@@ -14,26 +15,41 @@ const addPkmBtn = document.getElementById("add-pokemon"); //getting the add poke
 slctDropDown.addEventListener("change", displayPokemonImage)
 addPkmBtn.addEventListener("click", addPokemonToTeam);
 
-fetchPokemonNames()
+fetchPokemonNames();
 
 function displayPokemonImage(){
+
+    if(changedPokemon === true){ //if the user decides to switch the pokemon clear the options because they contain the moves of the previous pokemon
+        move1.length = 0;
+        move2.length = 0;
+        move3.length = 0;
+        move4.length = 0;
+
+        changedPokemon = false;
+    }
 
     const pokemonName = slctDropDown.value; //grabbing the pokemon that the user selected
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
     .then(resp=>resp.json())
     .then((mons)=>{
+        currPokemon.name = pokemonName;
+        currPokemon.type1 = mons.types[0].type.name;
         const learnedMoves = mons.moves;
+        currPokemon.image = mons.sprites.front_default;
         img.src = mons.sprites.front_default;
         img.style = "width:250px;height:250px;";
         p.textContent = `Name: ${pokemonName}\n`;
         p1.textContent = `Type 1: ${mons.types[0].type.name}`;
 
         if(mons.types.length === 1){ //checking if the pokemon has only 1 type. if so set type 2 as `NONE`
+            currPokemon.type2 = null;
             p2.textContent = `Type 2: None`;
         }else{ 
+            currPokemon.type2 = mons.types[1].type.name;
             p2.textContent = `Type 2: ${mons.types[1].type.name}`;
         }
 
+        currPokemon.ability = mons.abilities[0].ability.name;
         p3.textContent = `Ability: ${mons.abilities[0].ability.name}`;
 
         displayMoves(learnedMoves);
@@ -47,6 +63,7 @@ function displayMoves(moves){
         const option2 = document.createElement("option");
         const option3 = document.createElement("option");
         const option4 = document.createElement("option");
+
         option1.value = move.move.name;
         option1.textContent = move.move.name;
         option2.value = move.move.name;
@@ -60,11 +77,49 @@ function displayMoves(moves){
         move2.appendChild(option2);
         move3.appendChild(option3);
         move4.appendChild(option4);
+
+        changedPokemon = true;
     })
 }
 
 function addPokemonToTeam(){
     //each team can have a maximum of only 6 pokemon. do not let the user add more than 6 
+    if(checkIfMaxReached(pkmTeam1)){
+        //add the pokemon to a new team
+        console.log("TEAM FULL");
+    }else{
+        currPokemon.move1 = move1.value;
+        currPokemon.move2 = move2.value;
+        currPokemon.move3 = move3.value;
+        currPokemon.move4 = move4.value;
+
+        pkmTeam1.push(currPokemon);
+        displayTeam(pkmTeam1);
+
+        currPokemon = {};
+        
+    }
+}
+
+function displayTeam(arr){
+    
+    arr.forEach((pokemon, index)=>{ //challenge!
+        const grabDiv = document.getElementById("team1");
+        const img = document.createElement("img");
+        img.src = pokemon.image;
+        grabDiv.appendChild(img);
+    })
+}
+
+function checkIfMaxReached(arr){
+    let full;
+    if(arr.length === 6){
+        full = true;
+    }else{
+        full = false;
+    }
+
+    return full;
 }
 
 function fetchPokemonNames(){
